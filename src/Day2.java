aeb63507165d16214dfac647f5feabbccc30655f
michaela jangefalk <michaela.jangefalk@gmail.com>
2021-12-18 00:43:39 +0100

    Added method for calculating combined position with aim.

--- src/Day2.java
+++ src/Day2.java
@@ -8,6 +8,10 @@ public class Day2 {
     public static void run() throws IOException {
         // 2a. Dive!
         System.out.println("Combined position " + getMultipliedPosition());
+
+        // 2b. Dive with aim!
+        System.out.println("Combined position with aim " + getMultipliedPositionWithAim());
+
     }
 
     private static int getMultipliedPosition() throws IOException {
@@ -32,6 +36,37 @@ public class Day2 {
             }
         }
         System.out.format("Position horizontal: %d and depth %d", horizontal, depth);
+        System.out.println();
+
+        return horizontal * depth;
+    }
+
+    private static int getMultipliedPositionWithAim() throws IOException {
+        int horizontal = 0;
+        int depth = 0;
+        int aim = 0;
+
+        List<String> commands = utilities.getInputFromFile(filePath);
+
+        for (String s : commands) {
+            String[] command = s.split(" ");
+
+            switch (command[0]) {
+                case "forward":
+                    horizontal += Integer.parseInt(command[1]);
+                    depth += aim * Integer.parseInt(command[1]);
+                    break;
+                case "down":
+                    aim+= Integer.parseInt(command[1]);
+                    //depth += Integer.parseInt(command[1]);
+                    break;
+                case "up":
+                    aim-= Integer.parseInt(command[1]);
+                    // -= Integer.parseInt(command[1]);
+                    break;
+            }
+        }
+        System.out.format("Position horizontal: %d, depth %d and aim %d ", horizontal, depth, aim);
         System.out.println();
 
         return horizontal * depth;
