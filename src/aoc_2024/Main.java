29a45afed5246e8ec925d1f2d6ddba01043d5f6f
michaela <michaela.janegfalk@gmail.com>
2024-12-08 23:31:44 +0100

    Refactor Main

--- src/aoc_2024/Main.java
+++ src/aoc_2024/Main.java
@@ -1,23 +1,23 @@
 package aoc_2024;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		// Day 1
-		System.out.println("---------- Day 1 ----------");
-		Day day1 = new Day1();
-		System.out.println("--- Day 1 - Test run ---");
-		day1.test();
-		System.out.println("--- Day 1 - Run ---");
-		day1.run();
-
-		// Day 1
-		System.out.println("---------- Day 2 ----------");
-		Day day2 = new Day2();
-		System.out.println("--- Day 2 - Test run ---");
-		day2.test();
-		System.out.println("--- Day 2 - Run ---");
-		day2.run();
+		List<Day> days = new ArrayList<>();
+		days.add(new Day1());
+		days.add(new Day2());
+		//days.add(new Day3());
+		for(int i = 1; i < days.size(); i++) {
+			System.out.println(String.format("---------- Day %s ----------%n", i));
+			Day day = days.get(i);
+			System.out.println(String.format("--- Day %s - Test run ---", i));
+			day.test();
+			System.out.println(String.format("--- Day %s - Run ---", i));
+			day.run();
+		}
 	}
 }
